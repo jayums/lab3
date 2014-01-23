d536593fce110c319fa0b8706abfea8c562d42ae
James Guan <james.guan5693@gmail.com>
2014-01-23 10:29:46 -0800

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,51 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("HELLO WORLD!");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
+
+	$("a.thumbnail").click(projectClick);
+	$("#submitBtn").click(submitChangeForm);
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 }
+
+function projectClick(e) {
+  // Cancel the default action, which prevents the page from reloading
+    e.preventDefault();
+
+    // In an event listener, $(this) is the leement that fired the event
+    var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $(".jumbotron h1");
+    jumbotronHeader.text(projectTitle);
+
+    var containingProject = $(this).closest(".project"); 
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) { 
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>"); 
+    } else { 
+       $(description).fadeOut(function(e) {
+       	$(this).remove();
+       });
+    }
+}
+
+function submitChangeForm(e) {
+	e.preventDefault();
+	var project = $("#project").val();
+	var widthInPx = $("#width").val();
+	var description = $("#description").val();
+	$(project).animate({width: widthInPx + "px"}, 500);
+	$(project + " .project-description").text(description); 
+}
+
+
+
+
+
+
+
+
